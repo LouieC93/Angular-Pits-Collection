36d5884337633637dfbaf2db22475f576218a26d
Louie Chen <louieforcode@gmail.com>
2022-02-25 14:59:44 +0800

    feat: add home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CONSOLE } from '../core/encap-console';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
